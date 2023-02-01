7b3a63f763d18141d46e430a5e93a53a0bf7c50f
Max Hu <1853370994@qq.com>
2023-02-01 12:10:47 +0800

    template

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vue 3 + Vite Template
+
+vite-vue3-template
---
+++ src/components/MyTemplate.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {onMounted, onUnmounted} from "vue";
+
+
+onMounted(()=>{
+
+})
+
+onUnmounted(()=>{})
+
+</script>
+
+<template>
+
+
+</template>
+
+<style scoped>
+
+</style>
